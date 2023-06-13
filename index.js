dbc652bd01125aee9d1831084a6b397bbf5edfcf
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2023-06-13 15:36:38 +0600

    MongoDb SuccessFully Added

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//MongoDB server
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixy6gu6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // Database connection
+
+    const gamesCollection = client.db("mafuz_game_zone").collection("gameInfo");
+    const reviewsCollection = client
+      .db("mafuz_game_zone")
+      .collection("reviews");
+
+    // MongoDB Routes Start Here
+
+    // GameINfo
+    app.get("/gameinfo", async (req, res) => {
+      const result = await gamesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Mafuz-Game-Zone Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
